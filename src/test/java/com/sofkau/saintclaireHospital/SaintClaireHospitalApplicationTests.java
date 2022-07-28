70452a6e7cd74793d4016e050297e593d5b887a4
Drownfe <palaciojuanfe@gmail.com>
2022-07-28 11:48:14 -0500

    Project Created

---
+++ src/test/java/com/sofkau/saintclaireHospital/SaintClaireHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.saintclaireHospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintClaireHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
