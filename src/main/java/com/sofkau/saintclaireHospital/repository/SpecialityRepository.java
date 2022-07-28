34dc645beac1bfa3bcdf3d07a672b9cb2ef34ba5
Drownfe <palaciojuanfe@gmail.com>
2022-07-28 16:04:30 -0500

    Repository package with interfaces

---
+++ src/main/java/com/sofkau/saintclaireHospital/repository/SpecialityRepository.java
@@ -0,0 +1,9 @@
+package com.sofkau.saintclaireHospital.repository;
+
+import com.sofkau.saintclaireHospital.entity.Speciality;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpecialityRepository extends JpaRepository<Speciality, Long> {
+}
