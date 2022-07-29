6cd7673e2875f389c85126cad73a3e05e4342051
Drownfe <palaciojuanfe@gmail.com>
2022-07-29 14:19:37 -0500

    Adding exceptions and controller

--- src/main/java/com/sofkau/saintclaireHospital/SaintClaireHospitalApplication.java
+++ src/main/java/com/sofkau/saintclaireHospital/SaintClaireHospitalApplication.java
@@ -1,13 +1,26 @@
 package com.sofkau.saintclaireHospital;
 
+import com.sofkau.saintclaireHospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SaintClaireHospitalApplication {
-
+public class SaintClaireHospitalApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SaintClaireHospitalApplication.class, args);
+
 	}
 
+	@Autowired
+	private PatientRepository patientRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("********************************");
+		System.out.println("\t SAINT CLAIRE HOSPITAL");
+		System.out.println("********************************");
+		System.out.println();
+
+	}
 }
